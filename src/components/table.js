44820f7fc530e05e31d122bf711796f31ebdfdd5
UnaiH <71124187+UnaiH@users.noreply.github.com>
2022-05-05 14:26:47 +0200

    Hecho

---
+++ src/components/table.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+import Button from "./button"
+import Moment from "react-moment";
+
+const Table =({
+                  list,
+                  onDismiss}) =>
+    <div className="table">
+
+        {list.map(item =>
+        {return <div key={item.objectID} className="table-row">
+             <span style={{ width: '40%' }}>
+                <a href={item.url}>{item.title}</a>
+                </span>
+            <span style={{ width: '30%' }}>
+                {item.author}
+                </span>
+            <span style={{ width: '25%' }}>
+                <Moment fromNow>{item.created_at}</Moment>
+                        </span>
+            <span style={{ width: '10%' }}>
+                        <Button
+                            onClick={() => onDismiss(item.objectID)}
+                            className="button-inline"
+                        > Dismiss
+                        </Button>
+                        </span>
+        </div>;
+        })}
+    </div>
+export default Table;
